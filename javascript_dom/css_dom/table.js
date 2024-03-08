0c77cbda465f2bab45437cc8fec19bad48f86b84
flowerarms <129850802+flowerarms@users.noreply.github.com>
2024-03-08 20:23:26 +0800

    Update table.js

--- javascript_dom/css_dom/table.js
+++ javascript_dom/css_dom/table.js
@@ -6,10 +6,10 @@ function stripeTables(){
         rows = table[i].getElementsByTagName("tr");
         for(var j=0;j<rows.length;j++){
             if(odd ==true){                               //第一个tr即tr[0]odd为false，走else把odd改为true
-                addClass(row[j], "intro");  
+                addClass(row[j], "intro");                //使用addclass函数，置换文本css，方便以后调整时直接去css里调
             //  rows[j].style.backgroundColor = "#ffc";   //tr[1]的odd为true,走条件一将backgroundcolor改为#ffc
-                odd = false;                              //以此来实现偶数行变色的斑马效果。
-            }else{
+                odd = false;                              //
+            }else{                                        //以此来实现偶数行变色的斑马效果。
                 odd = true;
             }
         }
