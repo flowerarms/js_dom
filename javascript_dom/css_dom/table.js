6029ee77c8daffd7c311cd23af8e07c47cc5aa60
flowerarms <129850802+flowerarms@users.noreply.github.com>
2024-03-08 20:21:08 +0800

    Update table.js

--- javascript_dom/css_dom/table.js
+++ javascript_dom/css_dom/table.js
@@ -6,12 +6,23 @@ function stripeTables(){
         rows = table[i].getElementsByTagName("tr");
         for(var j=0;j<rows.length;j++){
             if(odd ==true){                               //第一个tr即tr[0]odd为false，走else把odd改为true
-                rows[j].style.backgroundColor = "#ffc";   //tr[1]的odd为true,走条件一将backgroundcolor改为#ffc
+                addClass(row[j], "intro");  
+            //  rows[j].style.backgroundColor = "#ffc";   //tr[1]的odd为true,走条件一将backgroundcolor改为#ffc
                 odd = false;                              //以此来实现偶数行变色的斑马效果。
             }else{
                 odd = true;
             }
         }
+    }
+}
+function addClass(element, value){
+    if(!element.className){
+        element.className = value;
+    }else{
+        newClassName = element.className;
+        newClassName += ' ';
+        newClassName += value;
+        element.className = newClassName;
     }
 }
 window.onload = function(){
